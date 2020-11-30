8830ea5c88076999105f0f77dc4a952fa33f31a7
Muskan-Parihar <75237596+Muskan-Parihar@users.noreply.github.com>
2020-11-30 11:49:53 +0530

    Update R_programmin_week3_assignment.R

--- R_programmin_week3_assignment.R
+++ R_programmin_week3_assignment.R
@@ -1,12 +1,28 @@
+##makeCcacheMatrix creates a matrix that can cache its own inverse
 makecacheMatrix <- function(a=matrix()){
 	i<- NULL
+##set the matrix
 	set<- function(b){
 		a<<-b
 		i<<-NULL
 	}
+##get the matrix
 	get<-function()a
 	setInverseOfMatrix<-function(inverse)
 	i<<-inverse
 	getInverseOfMatrix<-function()i
 	list(set=set,get=get,setInverseOfMatrix=setInverseOfMatrix,getInverseOfMatrix=getInverseOfMatrix)
+}
+
+##cacheSolve will commute the inverse of matrix returned by makeCacheMatrix
+cacheSolve <- function(a,...){
+	i<- a$getInverseOfMatrix()
+	if(!is.null(i)){
+	message("getting cached data")
+	return(i)
+	}
+	matrix<-x$get()
+	i<-solve(matrix,...)
+	a$setInverseOfMatrix(i)
+	i
 }
